e64087fe019566bfd98637d65d0f74f3f4683cf1
Dhi Aurrahman <dio@rockybars.com>
2022-02-10 11:28:24 +0700

    Add initial go wrapper
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- config-load-check/main.go
+++ config-load-check/main.go
@@ -1,7 +1,94 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/dio/envoy-test-tools/internal/archives"
+	"github.com/dio/envoy-test-tools/internal/downloader"
+	"github.com/dio/envoy-test-tools/internal/runner"
+	"github.com/mitchellh/go-homedir"
+	"sigs.k8s.io/yaml"
+)
+
+const (
+	funcEHomeEnvKey = "FUNC_E_HOME"
+)
+
+var config string
 
 func main() {
-	fmt.Println("config load check")
+	flag.StringVar(&config, "config", "", "config files")
+	flag.Parse()
+
+	if config == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	args, err := check(config)
+	if err != nil {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	funcEHome := os.Getenv(funcEHomeEnvKey)
+	if funcEHome == "" {
+		home, _ := homedir.Dir()
+		funcEHome = filepath.Join(home, ".func-e")
+	}
+
+	ctx, stop := context.WithTimeout(context.Background(), 30*time.Second)
+	defer stop()
+
+	// Use whatever func-e has downloaded, or download the default one.
+	archive := &archives.ConfigLoadCheck{VersionUsed: ""} // TODO(dio): Infer this from func-e.
+	binaryPath, err := downloader.DownloadVersionedBinary(ctx, archive, funcEHome)
+	if err != nil {
+		fmt.Printf("failed to download proxy binary: %v\n", err)
+		os.Exit(1)
+	}
+	binary, _ := runner.New(ctx, binaryPath, args, nil)
+	binary.Run()
+}
+
+func check(config string) ([]string, error) {
+	info, err := os.Stat(config)
+	if err != nil {
+		return nil, err
+	}
+	if info.IsDir() {
+		return []string{config}, nil
+	}
+	b, err := os.ReadFile(config)
+	if err != nil {
+		return nil, err
+	}
+
+	tmp, err := ioutil.TempDir("", "check")
+	if err != nil {
+		return nil, err
+	}
+
+	var name string
+	if b[0] == '{' {
+		name = "check_*.json"
+	} else {
+		name = "check_*.yaml"
+		b, err = yaml.YAMLToJSON(b)
+		if err != nil {
+			return nil, err
+		}
+	}
+	f, err := os.CreateTemp(tmp, name)
+	if err != nil {
+		return nil, err
+	}
+	_, err = f.Write(b)
+	return []string{tmp}, err
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/dio/envoy-test-tools
 
 go 1.17
+
+require (
+	github.com/bazelbuild/bazelisk v1.11.0
+	github.com/codeclysm/extract v2.2.0+incompatible
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/stretchr/testify v1.7.0
+	github.com/ulikunitz/xz v0.5.10
+	sigs.k8s.io/yaml v1.3.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/h2non/filetype v1.1.3 // indirect
+	github.com/juju/errors v0.0.0-20220203013757-bd733f3c86b9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
